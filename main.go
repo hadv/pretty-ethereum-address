5a68280427b6c2a47fc10c82add51daa79d6e03b
hadv <dvietha@gmail.com>
2019-02-07 16:47:17 +0700

    gen address that has 0x888888 prefix

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	for {
+		// Create an account
+		key, _ := crypto.GenerateKey()
+
+		// Get the address
+		address := crypto.PubkeyToAddress(key.PublicKey).Hex()
+		fmt.Println(address)
+
+		if strings.HasPrefix(address, "0x888888") {
+			// Get the private key
+			privateKey := hex.EncodeToString(key.D.Bytes())
+			fmt.Println(privateKey)
+			break
+		}
+	}
+}
