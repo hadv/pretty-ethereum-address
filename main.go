aac879fc7e2141e5e969b08ab7cf20d2cd814add
hadv <dvietha@gmail.com>
2019-03-15 22:32:38 +0700

    using quit channel to exit program

--- main.go
+++ main.go
@@ -11,21 +11,27 @@ import (
 )
 
 func main() {
+	quit := make(chan bool)
 	var wg sync.WaitGroup
 	wg.Add(5000)
 	for i := 0; i < 5000; i++ {
 		go func() {
 			defer wg.Done()
 			for {
-				// Create an account
-				key, _ := crypto.GenerateKey()
-				// Get the address
-				address := crypto.PubkeyToAddress(key.PublicKey).Hex()
-				if strings.HasSuffix(address, "31415926") {
-					// Get the private key
-					privateKey := hex.EncodeToString(key.D.Bytes())
-					fmt.Println(privateKey)
+				select {
+				case <-quit:
 					os.Exit(0)
+				default:
+					// Create an account
+					key, _ := crypto.GenerateKey()
+					// Get the address
+					address := crypto.PubkeyToAddress(key.PublicKey).Hex()
+					if strings.HasSuffix(address, "31415") {
+						// Get the private key
+						privateKey := hex.EncodeToString(key.D.Bytes())
+						fmt.Println(privateKey)
+						quit <- true
+					}
 				}
 			}
 		}()
